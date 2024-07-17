d0a99cca64a491fa6583ce65565c66afed4fafdf
Your GitHub username here <congtianyue233@gmail.com>
2024-07-17 19:28:09 -0400

    Updated upper bound of integration

--- model_fitting/util.py
+++ model_fitting/util.py
@@ -12,7 +12,7 @@ import pymc as pm
 
 def stop_respond_likelihood(t_r, mu_go, sigma_go, tau_go, 
                             mu_stop, sigma_stop, tau_stop, 
-                            p_tf, ssd, epsilon=1e-10):
+                            p_tf, ssd):
     """
     Calculate the log-likelihood for a stop-signal response time model 
     using Ex-Gaussian distributions.
@@ -57,40 +57,44 @@ def stop_respond_likelihood(t_r, mu_go, sigma_go, tau_go,
     
     likelihood = failed_trigger + successful_trigger
     
-    return pt.log(likelihood + epsilon)
+    return pt.log(likelihood)
 
-def legendre_quadrature_pytensor(upper_bound, ssd, n=50):
-    """
-    Perform Gauss-Legendre quadrature to approximate the 
-    definite integral of a function.
-
-    Parameters
-    ----------
-    upper_bound : array-like
-        Upper bound of the integration interval.
-    ssd : array-like
-        Stop-signal delays.
-    n : int, optional
-        Number of quadrature nodes, by default 50.
-
-    Returns
-    -------
-    tuple
-        Transformed nodes and weights for the quadrature.
-    """
+def legendre_quadrature_pytensor(ssd, upper_bound, n):
+    """Gauss-Legendre quadrature nodes and weights."""
     nodes, weights = np.polynomial.legendre.leggauss(n)
     nodes_pt, weights_pt = pt.constant(nodes), pt.constant(weights)
     
-    # Transform nodes from [-1, 1] (default for the legendre quadrature rule) 
-    # to [SSD, upper_bound]
-    transformed_nodes = 0.5 * (nodes_pt[:, None] + 1) * (upper_bound - ssd) + ssd
-    transformed_weights = weights_pt[:, None] * 0.5 * (upper_bound - ssd)
+    # Transform nodes from [-1, 1] to [lower_bound, upper_bound]
+    # lower_bound = pt.as_tensor_variable(lower_bound).dimshuffle('x')  # Expand dims to (1,)
+    lower_bound = ssd
+    upper_bound = pt.as_tensor_variable(upper_bound).dimshuffle('x')  # Expand dims to (1,)
+    
+    transformed_nodes = 0.5 * (nodes_pt[:, None] + 1) * (upper_bound - lower_bound) + lower_bound
+    transformed_weights = weights_pt[:, None] * 0.5 * (upper_bound - lower_bound)
 
     return transformed_nodes, transformed_weights
 
+def integrate_cexgauss(upper_bound, mu_go, sigma_go, tau_go, mu_stop, 
+                       sigma_stop, tau_stop, ssd, n):
+    """Numerical integration of the Censored Ex-Gaussian using Gauss-Legendre quadrature."""
+    nodes, weights = legendre_quadrature_pytensor(ssd, upper_bound, n)
+
+    # Derive the Ex-Gaussian PDF and CDF using PyMC functions
+    exgaussian_go = pm.ExGaussian.dist(mu=mu_go, sigma=sigma_go, nu=tau_go)
+    exgaussian_stop = pm.ExGaussian.dist(mu=mu_stop, sigma=sigma_stop, nu=tau_stop)
+
+    exgaussian_cdf_vals = pt.exp(pm.logcdf(exgaussian_go, nodes))
+    exgaussian_pdf_vals = pt.exp(pm.logp(exgaussian_stop, nodes - ssd))
+
+    integrands = (1 - exgaussian_cdf_vals) * exgaussian_pdf_vals
+    integrals = weights * integrands
+    total_integral = pt.sum(integrals, axis=0)
+
+    return total_integral
+
 def successful_inhibit_log_likelihood(mu_go, sigma_go, tau_go, 
                                       mu_stop, sigma_stop, tau_stop, 
-                                      p_tf, upper_bound, ssd, n=50, epsilon=1e-10):
+                                      p_tf, ssd, upper_bound=10000, n=100):
     """
     Calculate the log-likelihood for successful inhibition in a stop-signal 
     task using Legendre quadrature rule.
@@ -125,22 +129,9 @@ def successful_inhibit_log_likelihood(mu_go, sigma_go, tau_go,
     pytensor.tensor
         Log-likelihood of the successful inhibition given the parameters.
     """
-    # Derive the nodes and weights for Gauss-Legendre quadrature
-    nodes, weights = legendre_quadrature_pytensor(upper_bound, ssd, n)
-
-    # Derive the Ex-Gaussian PDF and CDF using PyMC functions
-    exgaussian_go = pm.ExGaussian.dist(mu=mu_go, sigma=sigma_go, nu=tau_go)
-    exgaussian_stop = pm.ExGaussian.dist(mu=mu_stop, sigma=sigma_stop, nu=tau_stop)
-
-    exgaussian_cdf_vals = pt.exp(pm.logcdf(exgaussian_go, nodes))
-    exgaussian_pdf_vals = pt.exp(pm.logp(exgaussian_stop, nodes - ssd))
-
-    # Sum (integration) of the product of transformed nodes and weights
-    integrands = (1 - exgaussian_cdf_vals) * exgaussian_pdf_vals
-    integrals = weights * integrands
-    total_integral = pt.sum(integrals, axis=0)
+    total_integral = integrate_cexgauss(upper_bound, mu_go, sigma_go, tau_go, mu_stop, sigma_stop, tau_stop, ssd, n)
 
     # Derive the likelihood for each trial
     likelihood = (1 - p_tf) * total_integral
-
-    return pt.log(likelihood + epsilon)
+    
+    return pt.log(likelihood)
