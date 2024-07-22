f5a8f931fb29c94ca961a5decdb613c7ef9e1384
Your GitHub username here <congtianyue233@gmail.com>
2024-07-22 14:03:18 -0400

    Removed unused imports

--- simulation/util.py
+++ simulation/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import random
-from scipy.stats import norm, exponnorm, truncnorm
+from scipy.stats import norm, exponnorm
 
 '''
 Functions related to simulation
